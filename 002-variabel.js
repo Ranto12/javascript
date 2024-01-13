d6803faf0bba509d372cb61e99344e30cef6cd18
ranto <rantoirwanda08@gmail.com>
2024-01-13 21:16:01 +0700

    operasi javascript

---
+++ 002-variabel.js
@@ -0,0 +1,25 @@
+// var x = 5;
+
+// function contohFungsi() {
+//     var x =10;
+//     console.log(x, "didalam fungsi")
+// }
+// contohFungsi()
+// var x = 100;
+// console.log(x, "diluar fungsi")
+
+// let y = 5;
+
+// if (true) {
+//     let y = 10;
+//     console.log(y)
+// }
+// console.log(y)
+
+const z = 10;
+
+if(true) {
+    const z = 200;
+    console.log(z)
+}
+console.log(z)
---
+++ 003-operasi.js
@@ -0,0 +1,66 @@
+// penjumlahan 
+const hasilPenjumlahan = 8 + 9;
+
+const nilaiAwal = 230;
+const nilaiKedua = 100;
+
+const hasilPenjumlahanAdanB = nilaiAwal + nilaiKedua;
+
+// pengurangan 
+
+const hasilPengurangan = 100 - 17;
+
+const nilaiA = 80;
+const nilaiB = 90;
+
+const hasilPenguranganAdanB = nilaiB - nilaiA;
+
+// perkalian 
+
+const hasilPerkalian = 4 * 5;
+
+const A = 90;
+const B= 2;
+
+const hasilPerkalianAdanB = A * B;
+
+// pembagian 
+const pembagian = 90/10;
+
+// modulus 
+
+const modulus = 90 % 7;
+
+// persamaan 
+
+const nilaipertama = 10;
+const nilaiyangKedua = '10';
+
+const perbandingan1 = nilaipertama === nilaiyangKedua;
+const perbandingan2 = nilaipertama == nilaiyangKedua;
+
+// concatination
+
+const text1 = "hallo ";
+const text2 = "dunia";
+
+const gabungan = text1 + 120;
+
+
+// incremen dan decrement 
+let angka = 10;
+
+angka++;
+angka++;
+angka++;
+angka++;
+angka--;
+angka--;
+angka--;
+angka--;
+angka--;
+angka--;
+
+// perpangakatan
+const pangkat = 10 ** 3;
+console.log(pangkat)
